ff3f4612667413dd93e3a766fd8210eeeed2e632
n <n@n.n>
2015-03-13 14:45:20 +0200

    sql jutut alotettu

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,2 +1,5 @@
 INSERT INTO Kayttaja (nimi, salasana) VALUES ('niku', 'joo');
 INSERT INTO Kayttaja (nimi, salasana) VALUES ('blaa', 'hmm');
+
+INSERT INTO Blogi (nimi, kenen, osoite) VALUES ('blogi vaan', 'niku', 'blogijuttu');
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,3 +2,28 @@ CREATE TABLE Kayttaja(
   nimi varchar(50) PRIMARY KEY,
   salasana varchar(50) NOT NULL
 );
+
+CREATE TABLE Blogi(
+  nimi varchar(50) PRIMARY KEY,
+  kenen varchar(50) NOT NULL REFERENCES Kayttaja(nimi),
+  osoite varchar(30) NOT NULL
+);
+
+CREATE TABLE Postaus(
+  id integer PRIMARY KEY,
+  blogi varchar(50) NOT NULL REFERENCES Blogi(nimi),
+  pvm Date NOT NULL,
+  otsikko varchar(200),
+  leipateksti TEXT NOT NULL,
+  julkaistu char NOT NULL
+);
+
+CREATE TABLE Kategoria(
+  nimi varchar(50) PRIMARY KEY,
+  kuvaus TEXT
+);
+
+CREATE TABLE PostauksenKategoria(
+  postausID integer NOT NULL REFERENCES Postaus(id),
+  kategoriannimi varchar(50) NOT NULL REFERENCES Kategoria(nimi)
+);
